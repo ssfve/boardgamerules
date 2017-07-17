1c74cabb77046d697207558f5e2184f3d6751e61
Elvin Lin <ssfvessfve@163.com>
2017-07-18 00:04:13 +0800

    upload

---
+++ dahufa/gameintro/gameIntro.event.js
@@ -0,0 +1,24 @@
+var header_title = '故事背景';
+
+//list_line = generate(array);
+//var html1 = table_seg.replace('%data%',list_line);
+
+nameEN = change_nameEN(nameEN);
+var pageTitle = nameCN + nameEN;
+$("title").html(pageTitle);
+
+
+
+
+part.forEach(function(val,index){
+	list_line += part[index]
+});
+
+final_html = '';
+var final_html = table_seg.replace('%data%',list_line);
+
+$(document).ready(function() {
+	$('#header_title').html(header_title);
+	$('#introtext').html(final_html);
+});
+
---
+++ dahufa/variables/title.variables.js
@@ -0,0 +1,4 @@
+var nameCN = '大护法';
+var nameEN = 'dahufa';
+var theme_color = '#C62828';
+var subcontent_color = '#FFEBEE';
