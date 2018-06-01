3f148d5def904a422e8a84b7ca4749fd7043dc30
Elvin Lin <ssfvessfve@163.com>
2018-06-02 00:31:39 +0800

    123

--- js/functions/detail.event.js
+++ js/functions/detail.event.js
@@ -1,24 +1,15 @@
 mui.init({
     subpages:[{
-      url:"https://jingyan.baidu.com/album/7908e85ccc67f7af481ad2c2.html?picindex=2",
-	    id:"setup",
-	    createNew: true,//是否重复创建同样id的webview，默认为false:不重复创建，直接显示
+        url:"https://jingyan.baidu.com/album/7908e85ccc67f7af481ad2c2.html?picindex=2",
+	    id:"jingyan",
 	    styles: {
-            top: '500px', //新页面顶部位置
-            bottom: '1000px', //新页面底部位置
-            width: '50%', //新页面宽度，默认为100%
-            height: '50%'
-        },
-	    show:{
-	      autoShow:true//页面loaded事件发生后自动显示，默认为true
-	    },
-	    waiting:{
-	      autoShow:true,//自动显示等待框，默认为true
-	      title:'正在加载...'
-	    }
+            top: '0px', //新页面顶部位置
+       }
     }],
 	swipeBack: false
 });
+
+
 function setScrollHeader() {
 	//$('#inittitle').html(players4);
 	$('#gameSetup').html(gameSetup);
@@ -57,7 +48,7 @@ mui.previewImage();
 				//console.log('in flow')
 				//generate_html(pageType);		
 			//}else{
-			generate_html(pageType);
+			//generate_html(pageType);
 			change_tab_theme(theme_color,e.detail.slideNumber);
 			//}
 		} else if(e.detail.slideNumber === 2) {
@@ -65,7 +56,7 @@ mui.previewImage();
 			//if(item3.querySelector('.mui-loading')) {
 				//generate_html(pageType);
 			//}else{
-			generate_html(pageType);
+			//generate_html(pageType);
 			change_tab_theme(theme_color,e.detail.slideNumber);
 			//}
 		} else if(e.detail.slideNumber === 3) {
@@ -76,7 +67,7 @@ mui.previewImage();
 			}
 		} else if(e.detail.slideNumber === 0) {
 			pageType = 'setup'
-			generate_html(pageType);
+			//generate_html(pageType);
 			change_tab_theme(theme_color,0);
 		}
 		//setTimeout(function() {
