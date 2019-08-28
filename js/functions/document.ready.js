f4b460e42954c3ba056ab56965d56287dbd278e0
MrRedHerring <ssfvessfve@163.com>
2019-08-28 07:43:47 +0000

    Update document.ready.js

--- js/functions/document.ready.js
+++ js/functions/document.ready.js
@@ -246,7 +246,7 @@ if (current_page === 'gamecover') {
         url: entry_query_url,
         success: function(data) {
             let mod_result = '';
-            console.log(type(data));
+            console.log(typeof data);
             for(i in data){
                 console.log(data[i]);
                 mod_result += data[i].gstone_id + "\n"
