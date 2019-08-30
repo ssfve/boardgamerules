03d18034cf145cf441f1fa736a182a0a99cdce55
MrRedHerring <ssfvessfve@163.com>
2019-08-30 07:45:28 +0000

    Update createPage_button_click.js

--- js/functions/createPage_button_click.js
+++ js/functions/createPage_button_click.js
@@ -1,4 +1,6 @@
 // create Page
+let fileName = '';
+
 $('#upload_background_button').on('click',function () {
 	console.log('upload_background_button clicked');
     $('#bg_img_file').click();
@@ -6,11 +8,9 @@ $('#upload_background_button').on('click',function () {
 
 $("#bg_img_file").on('change', function(e) {
 	console.log('bg_img_file changed');
+	fileName = e.target.files[0].name;
+	console.log(fileName);
 	$("#background_submit_form").submit();
-
-	let fileName = e.target.files[0].name;
-	$('body').css('background', `url('http://180.76.244.130:18000/${fileName}')no-repeat`);
-
 });
 
 // intervene submit
@@ -18,11 +18,12 @@ $('#background_submit_form').submit(function(e) {
 	e.preventDefault(); // Prevent the form from submitting via the browser
 	let form = $(this);
 	$.ajax({
+		async: false,
 		type: form.attr('method'),
 		url: form.attr('action'),
 		data: form.serialize()
 	}).done(function(data) {
-		// Optionally alert the user of success here...
+		$('body').css('background', `url('http://180.76.244.130:18000/${fileName}')no-repeat`);
 	}).fail(function(data) {
 		// Optionally alert the user of an error here...
 	});
