4433a3b5d08e8bffa26afbeb8b092d1ac9d137e5
MrRedHerring <ssfvessfve@163.com>
2019-09-20 06:49:04 +0000

    Update choosePage.click.js

--- js/functions/choosePage.click.js
+++ js/functions/choosePage.click.js
@@ -19,11 +19,27 @@ let callGetGuideId = function (button_id) {
         dataType: "json"
     }).done(function (guide_id) {
         console.log('Returning guide_id=' + guide_id);
-        getPageList(guide_id);
+        getButtonToPageId(button_id, guide_id);
     });
 };
 
-let getPageList = function (guide_id) {
+let getButtonToPageId = function (button_id, guide_id) {
+    $.ajax({
+        url: 'http://180.76.244.130:3000/database/getAttribute',
+        type: 'GET',
+        data: {
+            table_name: 'raw_button_table',
+            attribute_name: 'button_to_page_id',
+            key_name: 'button_id',
+            key_value: button_id
+        }
+    }).done(function (to_page_id) {
+        console.log(to_page_id);
+        getPageList(to_page_id, guide_id);
+    });
+};
+
+let getPageList = function (to_page_id, guide_id) {
     $.ajax({
         url: 'http://180.76.244.130:3000/guide/getPageList',
         type: 'GET',
@@ -33,7 +49,7 @@ let getPageList = function (guide_id) {
         }
     }).done(function (page_list) {
         console.log(page_list);
-        addListElement(guide_id, page_list);
+        addListElement(guide_id, page_list, to_page_id);
     });
 };
 
@@ -51,19 +67,17 @@ let getPreviousPageId = function (page_address_seg, button_id) {
 };
 
 let addSwitchCheck = function (page_id) {
-    document.getElementById(`#page_${page_id}`).addEventListener('tap', function () {
-        let btnArray = ['否', '是'];
-        mui.confirm('是否跳转至选择页面？(不跳转将直接返回)', 'Hello MUI', btnArray, function (e) {
-            if (e.index === 1) {
-                console.log('你刚确认跳转');
-                switchPage(page_address_seg, page_id);
-            } else {
-                console.log('你没有选择跳转，将返回');
-                // get previous page id first then switch
-                getPreviousPageId(page_address_seg, button_id);
-            }
-        })
-    });
+    let btnArray = ['否', '是'];
+    mui.confirm('是否跳转至选择页面？(不跳转将直接返回)', 'Hello MUI', btnArray, function (e) {
+        if (e.index === 1) {
+            console.log('你刚确认跳转');
+            switchPage(page_address_seg, page_id);
+        } else {
+            console.log('你没有选择跳转，将返回');
+            // get previous page id first then switch
+            getPreviousPageId(page_address_seg, button_id);
+        }
+    })
 };
 
 let saveBranchPageId = function (guide_id, page_id) {
@@ -86,8 +100,8 @@ let saveButtonAttribute = function (attribute_name, attribute_value, button_id) 
         type: 'GET',
         data: {
             attribute_name: attribute_name,
-            button_id: button_id,
-            attribute_value: attribute_value
+            attribute_value: attribute_value,
+            button_id: button_id
         }
     }).done(function (data) {
         console.log('returning result is ' + data);
@@ -96,10 +110,10 @@ let saveButtonAttribute = function (attribute_name, attribute_value, button_id) 
 
 let saveAttribute = function (table_name, attribute_name, attribute_value, key_name, key_value) {
     $.ajax({
-        url: 'http://180.76.244.130:3000/database/saveAttribute',
+        url: 'http://180.76.244.130:3000/database/updateAttribute',
         type: 'GET',
         data: {
-            table: table_name,
+            table_name: table_name,
             attribute_name: attribute_name,
             attribute_value: attribute_value,
             key_name: key_name,
@@ -124,9 +138,27 @@ let createBranchPage = function (guide_id) {
     });
 };
 
-let getTextContent = function (list_element, page_id, text_id) {
+let addListEvent = function (page_id) {
+    let list_element = $(`#page_${page_id}`);
+    list_element.on('tap', list_element, function () {
+        //first to do is to save button info
+        //element is to page id
+        let button_text = $('#desc_input').val();
+        if (button_text === '' || button_text === undefined) {
+            button_text = 'BTN-DFT-TXT'
+        }
+        console.log(button_text);
+        saveAttribute('raw_button_table', 'button_text', button_text, 'button_id', button_id);
+        saveButtonAttribute('button_to_page_id', page_id, button_id);
+        console.log(page_id);
+        addSwitchCheck(page_id);
+    });
+};
+
+let getTextContent = function (list_element, page_id, text_id, to_page_id) {
     if (text_id === null || text_id === '' || text_id === undefined) {
         list_element.append(`<li class="mui-table-view-cell" id="page_${page_id}"><a class="mui-navigate-right">跳转页面编号${page_id}</a></li>`);
+        addListEvent(page_id);
     } else {
         $.ajax({
             url: 'http://180.76.244.130:3000/text/getTextAttribute',
@@ -139,11 +171,15 @@ let getTextContent = function (list_element, page_id, text_id) {
             console.log('returning text content is ' + text_content);
             list_element.append(`<li class="mui-table-view-cell" id="page_${page_id}"><a class="mui-navigate-right">跳转页面编号${page_id}-${text_content}</a></li>`);
             // return only after this is appended
+            addListEvent(page_id);
         });
+    }
+    if(page_id === to_page_id){
+        $(`#page_${page_id}`).css("color", "red")
     }
 };
 
-let getTextInfo = function (list_element, page_id) {
+let getTextInfo = function (list_element, page_id, to_page_id) {
     // get text id from specific page id
     $.ajax({
         url: 'http://180.76.244.130:3000/page/getPageAttribute',
@@ -154,11 +190,11 @@ let getTextInfo = function (list_element, page_id) {
         }
     }).done(function (text_id) {
         console.log('returning text_id=' + text_id);
-        getTextContent(list_element, page_id, text_id);
+        getTextContent(list_element, page_id, text_id, to_page_id);
     });
 };
 
-let addListElement = function (guide_id, page_list) {
+let addListElement = function (guide_id, page_list, to_page_id) {
     let list_element = $("#guide_page_list");
     let page_array = page_list.split(",");
     console.log(page_array);
@@ -166,20 +202,11 @@ let addListElement = function (guide_id, page_list) {
         console.log('processing page_' + element);
         // there are two page_id here
         // get text Content from text id
-        getTextInfo(list_element, element);
+        getTextInfo(list_element, element, to_page_id);
         // add tap listener for list element
-        $(`#page_${element}`).on('tap', function () {
-            //first to do is to save button info
-            let button_text = $('#desc_input').val();
-            if (button_text === '' || button_text === undefined) {
-                button_text = 'BTN-DFT-TXT'
-            }
-            console.log(button_text);
-            saveAttribute('raw_button_table', 'button_text', button_text, 'button_id', button_id);
-            saveButtonAttribute('button_to_page_id', page_id, button_id);
-            addSwitchCheck(page_id);
-        });
+
     });
+    // add events
     // add last create new page
     list_element.append(`<li class="mui-table-view-cell" id="default_new_page"><a class="mui-navigate-right">创建新页面</a></li>`);
     $("#default_new_page").on('tap', function () {
@@ -209,5 +236,6 @@ let addListElement = function (guide_id, page_list) {
 };
 
 callGetGuideId(button_id);
+
 
 
