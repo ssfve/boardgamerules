14f77c6dcb28809e2fa86664b3038675cacf722f
MrRedHerring <ssfvessfve@163.com>
2019-10-04 16:56:18 +0800

    add change

--- template/old/rule.variables.js
+++
@@ -1,4 +0,0 @@
-var nameEN = 'monopoly';
-var nameCN = '大富翁';
-var header_title = '主题概念';
-var array=[];
--- template4/variables/title.variables.js
+++
@@ -1,4 +0,0 @@
-var nameCN = '马尼拉';
-var nameEN = 'Manila';
-var theme_color = '#2E7D32';
-var subcontent_color = '#E8F5E9';
